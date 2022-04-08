b9455797968c17006337c98b4b945c4f3d52d564
inman <sf990668852@gmail.com>
2022-04-08 15:06:17 +0800

    update

--- Python GUI設計活用tkinter之路王者歸來第三版/ch4_Button/ch4_11.py
+++ Python GUI設計活用tkinter之路王者歸來第三版/ch4_Button/ch4_11.py
@@ -1,6 +1,10 @@
 # ch4_11.py
 from tkinter import *
 
+import os
+
+dirname = os.path.dirname(__file__)
+
 def msgShow():
     label.config(text="I love Python",bg="lightyellow",fg="blue")
       
@@ -8,7 +12,7 @@ root = Tk()
 root.title("ch4_11")                                # 視窗標題
 label = Label(root)                                 # 標籤內容
 
-sunGif = PhotoImage(file="sun.gif")                 # Image物件
+sunGif = PhotoImage(file=dirname+"/sun.gif")                 # Image物件
 btn = Button(root,image=sunGif,command=msgShow,     # 含影像的按鈕
              cursor="star")                         # star外形   
 label.pack()                      
--- Python GUI設計活用tkinter之路王者歸來第三版/ch4_Button/ch4_6.py
+++ Python GUI設計活用tkinter之路王者歸來第三版/ch4_Button/ch4_6.py
@@ -1,6 +1,9 @@
 # ch4_6.py
 from tkinter import *
 
+import os
+dirname = os.path.dirname(__file__)
+
 def msgShow():
     label.config(text="I love Python",bg="lightyellow",fg="blue")
       
@@ -8,7 +11,7 @@ root = Tk()
 root.title("ch4_6")                                 # 視窗標題
 label = Label(root)                                 # 標籤內容
 
-sunGif = PhotoImage(file="sun.gif")                 # Image物件
+sunGif = PhotoImage(file=dirname+"/sun.gif")                 # Image物件
 btn = Button(root,image=sunGif,command=msgShow)     # 含影像的按鈕
 label.pack()                      
 btn.pack()
